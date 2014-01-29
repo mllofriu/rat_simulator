bc42986a77429b1a5428e043ae5aa76bfc6a3e6c
mllofriu <mllofriu@gmail.com>
2014-01-29 13:25:40 -0500

    Test experiment to quickly test plotting mechanisms

--- src/experiment/multiscalemorris/MSMExperiment.java
+++ src/experiment/multiscalemorris/MSMExperiment.java
@@ -1,7 +1,9 @@
 package experiment.multiscalemorris;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Hashtable;
 import java.util.Map;
 
@@ -66,8 +68,22 @@ public class MSMExperiment extends Experiment {
 
 		// Execute the plotting script
 		try {
-			Process plot = Runtime.getRuntime().exec("r --no-save < plot.r",
+			System.out.println("Executing plotting scripts");
+			Process plot = Runtime.getRuntime().exec("Rscript plot.r",
 					null, new File(getLogPath()));
+			BufferedReader in = new BufferedReader(new InputStreamReader(
+					plot.getInputStream()));
+			String line = null;
+			while ((line = in.readLine()) != null) {
+				System.out.println(line);
+			}
+			
+			BufferedReader err = new BufferedReader(new InputStreamReader(
+					plot.getErrorStream()));
+			line = null;
+			while ((line = err.readLine()) != null) {
+				System.out.println(line);
+			}
 			plot.waitFor();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
