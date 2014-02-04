66de9981edfe20722445e391d623630d0fded942
mllofriu <mllofriu@gmail.com>
2014-02-04 10:58:12 -0500

    Successfully executed in circe

--- exec.sh
+++ exec.sh
@@ -1,10 +1,9 @@
 #$ -cwd
 #$ -N rat_sim
-#$ -l h_rt=08:00:00
+#$ -l h_rt=08:00:00,pcpus=8,location=tpa
 
+sh compile.sh
 
-export PATH="/usr/lib64/qt-3.3/bin:/opt/sge/bin:/opt/sge/bin/lx-amd64:/bin:/usr/bin:/apps/bin:/usr/lib64/alliance/bin:/home/m/mllofriualon/bin"
-export LD_LIBRARY_PATH="/usr/lib64/"
 Xvfb :1 -screen 2 1600x1200x16 &
 xvfb_pid=$!
 DISPLAY=:1.2 java  -Xmx4000m -Djava.library.path=/home/m/mllofriualon/java_libs/ -cp .:/home/m/mllofriualon//java_libs/commons-io-2.4.jar:/home/m/mllofriualon//java_libs/j3dcore.jar:/home/m/mllofriualon//java_libs/j3dutils.jar:/home/m/mllofriualon//java_libs/tcljava1.4.1/jacl.jar:/home/m/mllofriualon//java_libs/tcljava1.4.1/tcljava.jar:/home/m/mllofriualon//java_libs/vecmath.jar:bin/:src/ edu.usf.ratsim.experiment.multiscalemorris.MSMExperiment
