31ff75a588b79d3b182339142a978a7ac2967797
mllofriu <mllofriu@gmail.com>
2014-02-11 10:40:58 -0500

    minor changes in execution and plotting

--- exec.sh
+++ exec.sh
@@ -9,6 +9,6 @@ sh compile.sh
 Xvfb :1 -screen 2 1600x1200x16 &
 xvfb_pid=$!
 export DISPLAY=:1.2 
-java  -Xmx4000m -Djava.library.path=/home/m/mllofriualon/java_libs/ -cp .:/home/m/mllofriualon//java_libs/commons-io-2.4.jar:/home/m/mllofriualon//java_libs/j3dcore.jar:/home/m/mllofriualon//java_libs/j3dutils.jar:/home/m/mllofriualon//java_libs/tcljava1.4.1/jacl.jar:/home/m/mllofriualon//java_libs/tcljava1.4.1/tcljava.jar:/home/m/mllofriualon//java_libs/vecmath.jar:bin/:src/ edu.usf.ratsim.experiment.multiscalemorris.MSMExperiment
+java  -Xmx4000m -Djava.library.path=/work/m/mllofriualon/java_libs/ -cp .:/work/m/mllofriualon//java_libs/commons-io-2.4.jar:/work/m/mllofriualon//java_libs/j3dcore.jar:/work/m/mllofriualon//java_libs/j3dutils.jar:/work/m/mllofriualon//java_libs/tcljava1.4.1/jacl.jar:/work/m/mllofriualon//java_libs/tcljava1.4.1/tcljava.jar:/work/m/mllofriualon//java_libs/vecmath.jar:bin/:src/ edu.usf.ratsim.experiment.multiscalemorris.MSMExperiment
 
 kill $xvfb_pid
--- src/edu/usf/ratsim/experiment/plot/plot.sh
+++ src/edu/usf/ratsim/experiment/plot/plot.sh
@@ -4,4 +4,4 @@ mkdir -p plots/path/
 mkdir -p plots/policy/
 mkdir -p plots/runtime/
 
-Rscript plotting.r
+time Rscript plotting.r
