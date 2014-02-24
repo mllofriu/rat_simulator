6bb84f6106d31d17e19b2e71f0b2075268f001b5
mllofriu <mllofriu@gmail.com>
2014-02-24 17:59:20 -0500

    Changed to normal update instead of using the reverse one for simplicity sake.
    Fixed learning for multifeeders. However, it still seems to have problems to reach the food. The source is more likely to be the feeder mechanism, rather than navigational issues, since the rat reaches the feeder usually.
    
    
    Former-commit-id: eaded86e66caa853ddcd2c3c425f38edd75725fc

--- src/edu/usf/ratsim/experiment/model/MultiScaleMultiIntentionModel.java
+++ src/edu/usf/ratsim/experiment/model/MultiScaleMultiIntentionModel.java
@@ -9,28 +9,29 @@ import nslj.src.lang.NslModule;
 import org.w3c.dom.Element;
 
 import edu.usf.ratsim.experiment.ExperimentUniverse;
-import edu.usf.ratsim.nsl.modules.ArtificialPlaceCellLayer;
+import edu.usf.ratsim.nsl.modules.ArtificialPlaceCellLayerWithIntention;
 import edu.usf.ratsim.nsl.modules.GoalDecider;
 import edu.usf.ratsim.nsl.modules.TaxicFoodFinderSchema;
 import edu.usf.ratsim.nsl.modules.qlearning.QLSupport;
 import edu.usf.ratsim.nsl.modules.qlearning.actionselection.ProportionalExplorer;
 import edu.usf.ratsim.nsl.modules.qlearning.actionselection.SingleLayerAS;
-import edu.usf.ratsim.nsl.modules.qlearning.update.ReverseUpdate;
+import edu.usf.ratsim.nsl.modules.qlearning.update.NormalUpdate;
 import edu.usf.ratsim.robot.IRobot;
 
-public class MultiScaleModel extends NslModel implements RLRatModel {
-	private List<ArtificialPlaceCellLayer> pcls;
-	private List<ReverseUpdate> qLUpdVal;
+public class MultiScaleMultiIntentionModel extends NslModel implements
+		RLRatModel {
+	private List<ArtificialPlaceCellLayerWithIntention> pcls;
+	private List<NormalUpdate> qLUpdVal;
 	private ProportionalExplorer actionPerformerVote;
 	private List<SingleLayerAS> qLActionSel;
 	private List<QLSupport> qlData;
 	private GoalDecider goalD;
 	private TaxicFoodFinderSchema taxicDrive;
 
-	public MultiScaleModel(Element params, IRobot robot,
+	public MultiScaleMultiIntentionModel(Element params, IRobot robot,
 			ExperimentUniverse universe) {
-		super("MSModel", (NslModule) null);
-		
+		super("MSMIModel", (NslModule) null);
+
 		goalD = new GoalDecider("GoalDecider", this, universe);
 
 		// Get some configuration values for place cells + qlearning
@@ -40,9 +41,12 @@ public class MultiScaleModel extends NslModel implements RLRatModel {
 				.getElementsByTagName("maxRadius").item(0).getTextContent());
 		int numLayers = Integer.parseInt(params
 				.getElementsByTagName("numLayers").item(0).getTextContent());
+		int numIntentions = Integer
+				.parseInt(params.getElementsByTagName("numIntentions").item(0)
+						.getTextContent());
 
-		pcls = new LinkedList<ArtificialPlaceCellLayer>();
-		qLUpdVal = new LinkedList<ReverseUpdate>();
+		pcls = new LinkedList<ArtificialPlaceCellLayerWithIntention>();
+		qLUpdVal = new LinkedList<NormalUpdate>();
 		qLActionSel = new LinkedList<SingleLayerAS>();
 		qlData = new LinkedList<QLSupport>();
 
@@ -50,25 +54,26 @@ public class MultiScaleModel extends NslModel implements RLRatModel {
 		float radius = minRadius;
 		// For each layer
 		for (int i = 0; i < numLayers; i++) {
-			ArtificialPlaceCellLayer pcl = new ArtificialPlaceCellLayer(
-					"PlaceCellLayer", this, universe, radius);
+			ArtificialPlaceCellLayerWithIntention pcl = new ArtificialPlaceCellLayerWithIntention(
+					"PlaceCellLayer", this, universe, numIntentions, radius);
 			QLSupport qlSupport = new QLSupport(pcl.getSize());
 			pcls.add(pcl);
 			qlData.add(qlSupport);
-			qLActionSel.add(new SingleLayerAS("QLActionSel", this,
-					qlSupport, pcl.getSize(), robot, universe));
+			qLActionSel.add(new SingleLayerAS("QLActionSel", this, qlSupport,
+					pcl.getSize(), robot, universe));
 			// Update radius
 			radius += (maxRadius - minRadius) / (numLayers - 1);
 		}
 		// Created first to let Qlearning execute once when there is food
 		actionPerformerVote = new ProportionalExplorer("ActionPerformer", this,
 				numLayers, robot, universe);
-		
+
 		// Create taxic driver to override in case of flashing
-		taxicDrive = new TaxicFoodFinderSchema("Taxic Driver", this, robot, universe);
+		taxicDrive = new TaxicFoodFinderSchema("Taxic Driver", this, robot,
+				universe);
 
 		for (int i = 0; i < numLayers; i++) {
-			qLUpdVal.add(new ReverseUpdate("QLUpdVal", this, pcls.get(i)
+			qLUpdVal.add(new NormalUpdate("QLUpdVal", this, pcls.get(i)
 					.getSize(), qlData.get(i), robot, universe));
 		}
 	}
@@ -84,6 +89,8 @@ public class MultiScaleModel extends NslModel implements RLRatModel {
 			nslConnect(pcls.get(i), "activation", qLActionSel.get(i), "states");
 			nslConnect(qLActionSel.get(i).actionVote,
 					actionPerformerVote.votes[i]);
+			nslConnect(goalD.goalFeeder, pcls.get(i).goalFeeder);
+			nslConnect(pcls.get(i), "activation", qLUpdVal.get(i), "states");
 		}
 		nslConnect(goalD.goalFeeder, taxicDrive.goalFeeder);
 	}
@@ -92,11 +99,11 @@ public class MultiScaleModel extends NslModel implements RLRatModel {
 		return actionPerformerVote;
 	}
 
-	public List<ReverseUpdate> getQLValUpdaters() {
+	public List<NormalUpdate> getQLValUpdaters() {
 		return qLUpdVal;
 	}
 
-	public List<ArtificialPlaceCellLayer> getPCLLayers() {
+	public List<ArtificialPlaceCellLayerWithIntention> getPCLLayers() {
 		return pcls;
 	}
 
--- src/edu/usf/ratsim/nsl/modules/qlearning/QLSupport.java
+++ src/edu/usf/ratsim/nsl/modules/qlearning/QLSupport.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 import edu.usf.ratsim.nsl.modules.ArtificialPlaceCell;
 import edu.usf.ratsim.nsl.modules.ArtificialPlaceCellLayer;
+import edu.usf.ratsim.nsl.modules.ArtificialPlaceCellLayerWithIntention;
 import edu.usf.ratsim.support.Configuration;
 import edu.usf.ratsim.support.Utiles;
 
@@ -19,38 +20,38 @@ public class QLSupport {
 	private static final Float INITIAL_VALUE = 0f;
 	private static final String DUMP_FILENAME = "policy.txt";
 
-	private HashMap<StateAction, Float> value;
+	private HashMap<StateActionReward, Float> value;
 
-	private LinkedList<StateAction> visitedStateActions;
+	private LinkedList<StateActionReward> visitedStateActions;
 	private int numStates;
 	private static PrintWriter writer = null;
 
 	public QLSupport(int numStates) {
-		value = new HashMap<StateAction, Float>(numStates
+		value = new HashMap<StateActionReward, Float>(numStates
 				* Utiles.discreteAngles.length);
 		for (int s = 0; s < numStates; s++)
 			for (int a = 0; a < Utiles.discreteAngles.length; a++)
-				value.put(new StateAction(s, a), INITIAL_VALUE);
+				value.put(new StateActionReward(s, a), INITIAL_VALUE);
 
 		this.numStates = numStates;
 
-		visitedStateActions = new LinkedList<StateAction>();
+		visitedStateActions = new LinkedList<StateActionReward>();
 	}
 
-	public float getValue(StateAction sa) {
+	public float getValue(StateActionReward sa) {
 		return value.get(sa);
 	}
 
-	public void setValue(StateAction sa, float newVal) {
+	public void setValue(StateActionReward sa, float newVal) {
 		value.put(sa, newVal);
 	}
 
-	public void recordStateAction(StateAction sa) {
+	public void recordStateAction(StateActionReward sa) {
 		// Add to the front to reverse update later
 		visitedStateActions.add(0, sa);
 	}
 
-	public StateAction getVisitedSA(int index) {
+	public StateActionReward getVisitedSA(int index) {
 		return visitedStateActions.get(index);
 	}
 
@@ -93,6 +94,52 @@ public class QLSupport {
 
 	}
 
+	/**
+	 * Dumps the qlearning policy with a certain intention to a file. The
+	 * alignment between pcl cells and ql states is assumed for efficiency
+	 * purposes.
+	 * 
+	 * @param rep
+	 * @param subName
+	 * @param trial
+	 * @param rep
+	 * 
+	 * @param writer
+	 * @param pcl
+	 */
+	public void dumpPolicy(String trial, String groupName, String subName,
+			String rep, ArtificialPlaceCellLayerWithIntention pcl, int layer,
+			int numIntentions) {
+		synchronized (QLSupport.class) {
+			PrintWriter writer = QLSupport.getWriter();
+			for (int intention = 0; intention < numIntentions; intention++) {
+				
+
+				List<ArtificialPlaceCell> cells = pcl.getCells(intention);
+				for (int activeState = 0; activeState < cells.size(); activeState++) {
+					// Get the policy angle for this state
+					// Offset with the intention
+					int angle = getMaxAngle(intention * cells.size() + activeState);
+
+					// Write to file
+					String policyAngle;
+					if (angle == -1)
+						policyAngle = "NA";
+					else
+						policyAngle = new Float(Utiles.discreteAngles[angle])
+								.toString();
+
+					writer.println(trial + '\t' + groupName + '\t' + subName
+							+ '\t' + rep + '\t'
+							+ cells.get(activeState).getCenter().x + "\t"
+							+ (-cells.get(activeState).getCenter().z) + "\t"
+							+ policyAngle + "\t" + layer + '\t' + intention);
+				}
+			}
+		}
+
+	}
+
 	private static PrintWriter getWriter() {
 		if (writer == null) {
 			try {
@@ -100,7 +147,7 @@ public class QLSupport {
 						new FileOutputStream(new File(Configuration
 								.getString("Log.DIRECTORY") + DUMP_FILENAME))),
 						true);
-				writer.println("trial\tgroup\tsubject\trepetition\tx\ty\tangle\tlayer");
+				writer.println("trial\tgroup\tsubject\trepetition\tx\ty\tangle\tlayer\tintention");
 			} catch (FileNotFoundException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -109,13 +156,13 @@ public class QLSupport {
 
 		return writer;
 	}
-
+	
 	public int getMaxAngle(int s) {
 		float[] vals = new float[Utiles.discreteAngles.length];
 		int maxAngle = -1;
 		float maxVal = 0;
 		for (int angle = 0; angle < Utiles.discreteAngles.length; angle++) {
-			vals[angle] = getValue(new StateAction(s, angle));
+			vals[angle] = getValue(new StateActionReward(s, angle));
 			if (vals[angle] > maxVal) {
 				maxVal = vals[angle];
 				maxAngle = angle;
@@ -130,6 +177,10 @@ public class QLSupport {
 
 	public void clearRecord() {
 		visitedStateActions.clear();
+	}
+
+	public void popLastRecord() {
+		visitedStateActions.pollLast();
 	}
 
 }
