09891af4d544b60641c147db690ffb19235d9fd7
mllofriu <mllofriu@gmail.com>
2014-02-27 16:29:23 -0500

    Runned multifeeder training and recall with single layer succesfully.

--- src/edu/usf/ratsim/experiment/task/FlashFeeder.java
+++ src/edu/usf/ratsim/experiment/task/FlashFeeder.java
@@ -4,8 +4,6 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
 
-import com.sun.org.apache.bcel.internal.generic.NEW;
-
 import edu.usf.ratsim.experiment.ExperimentTask;
 import edu.usf.ratsim.experiment.ExperimentUniverse;
 import edu.usf.ratsim.experiment.subject.ExpSubject;
