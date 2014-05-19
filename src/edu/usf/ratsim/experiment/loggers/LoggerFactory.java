2ad30e7d0c1caf47a580562cbba5047f26fe3ab9
mllofriu <mllofriu@gmail.com>
2014-05-19 16:12:03 -0400

    Added logging for the wanted feeder

--- src/edu/usf/ratsim/experiment/loggers/LoggerFactory.java
+++ src/edu/usf/ratsim/experiment/loggers/LoggerFactory.java
@@ -37,6 +37,13 @@ public class LoggerFactory {
 //				res.add(new PolicyDumper(((MultiScaleModel) t.getSubject()
 //						.getModel()), t.getName(), t.getGroup(), t
 //						.getSubjectName(), t.getRep()));
+			} else if (loggerName.equals("WantedFeederLogger")) {
+				res.add(new WantedFeederLogger(t.getName(), t.getGroup(), t
+						.getSubjectName(), t.getRep()));
+//			} else if (loggerName.equals("PolicyDumper")) {
+//				res.add(new PolicyDumper(((MultiScaleModel) t.getSubject()
+//						.getModel()), t.getName(), t.getGroup(), t
+//						.getSubjectName(), t.getRep()));
 			} else if (loggerName.equals("PolicyDumperWithIntention")) {
 				res.add(new PolicyDumperWithIntention(
 						((RLRatModel) t.getSubject()
