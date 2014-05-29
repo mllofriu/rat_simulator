5df51046bd6f49ea49a2488c2ca4d87b324b1f19
mllofriu <mllofriu@gmail.com>
2014-05-28 20:02:41 -0400

    Changed the action selection of MaxVotes to select the max value for each action, instead of the best state first
    Former-commit-id: e75f8583e315cfdb0d48e1c1358f1dc9e100c63c

--- src/edu/usf/ratsim/nsl/modules/qlearning/actionselection/ProportionalMaxVotes.java
+++ src/edu/usf/ratsim/nsl/modules/qlearning/actionselection/ProportionalMaxVotes.java
@@ -25,42 +25,16 @@ public class ProportionalMaxVotes extends NslModule {
 	}
 
 	public void simRun() {
-		// Find the max action
-		int bestActionState = 0;
-		float bestActionValue = Float.NEGATIVE_INFINITY;
-		for (int state = 0; state < states.getSize(); state++)
-			for (int action = 0; action < numActions; action++) {
+		// Find the best value for each action, taking into acount state activation
+		for (int action = 0; action < numActions; action++) {
+			float bestActionValue = Float.NEGATIVE_INFINITY;
+			for (int state = 0; state < states.getSize(); state++)
 				if (states.get(state) * value.get(state, action) > bestActionValue) {
-					bestActionState = state;
 					bestActionValue = states.get(state)
 							* value.get(state, action);
 				}
-			}
-
-		// Set the votes of the state that gave the best action
-		for (int action = 0; action < numActions; action++) {
-			actionVote.set(
-					action,
-					states.get(bestActionState)
-							* value.get(bestActionState, action));
+			actionVote.set(action, bestActionValue);
 		}
-	}
-
-	private void setVotes(int state) {
-
-	}
-
-	private int getActiveState() {
-		// Winner take all within the layer
-		float maxVal = 0;
-		int activeState = -1;
-		for (int i = 0; i < states.getSize(); i++)
-			if (states.get(i) > maxVal) {
-				activeState = i;
-				maxVal = states.get(i);
-			}
-
-		return activeState;
 	}
 
 }
