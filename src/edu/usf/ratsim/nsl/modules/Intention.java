3b056950ab91cac51dcf0c79ac95db55015e9fc7
Martin Llofriu <mllofriu@gmail.com>
2014-06-29 19:48:20 -0400

    Added variable numbers of threads. Checked that the correct models are being called, and it seems so.
    Former-commit-id: 5e5b2e0a8eaba5003ff392760113dbd091632bd9

--- src/edu/usf/ratsim/nsl/modules/Intention.java
+++ src/edu/usf/ratsim/nsl/modules/Intention.java
@@ -8,22 +8,22 @@ public class Intention extends NslModule {
 
 	public NslDinInt0 goalFeeder;
 	public NslDoutFloat1 intention;
-	
-	public Intention(NslModule parent, String nslName, int numIntentions){
+
+	public Intention(NslModule parent, String nslName, int numIntentions) {
 		super(nslName, parent);
-		
+
 		goalFeeder = new NslDinInt0(this, "goalFeeder");
 		intention = new NslDoutFloat1(this, "intention", numIntentions);
 	}
-	
-	public void simRun(){
+
+	public void simRun() {
 		intention.set(0);
-		
+
 		if (goalFeeder.get() != -1)
 			intention.set(goalFeeder.get(), 1);
-//		for(int i = 0; i < intention.getSize(); i++)
-//			System.out.print(intention.get(i) + " ");
-//		System.out.println();
-		
+		// for(int i = 0; i < intention.getSize(); i++)
+		// System.out.print(intention.get(i) + " ");
+		// System.out.println();
+
 	}
 }
