a93261a3cf840fddbdf228f758dd53257f5e9946
Martin Llofriu <mllofriu@gmail.com>
2014-06-29 21:51:43 -0400

    added plotting back
    Former-commit-id: b8db734f434fe38ce2f12b4fa86a614057bf16f5

--- src/edu/usf/ratsim/experiment/Experiment.java
+++ src/edu/usf/ratsim/experiment/Experiment.java
@@ -8,18 +8,18 @@ package edu.usf.ratsim.experiment;
  * Fecha: 11 de agosto de 2010
  */
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Hashtable;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 import javax.vecmath.Point4f;
@@ -28,9 +28,6 @@ import org.apache.commons.io.FileUtils;
 import org.w3c.dom.Document;
 
 import edu.usf.ratsim.experiment.subject.ExpSubject;
-import edu.usf.ratsim.robot.IRobot;
-import edu.usf.ratsim.robot.RobotFactory;
-import edu.usf.ratsim.robot.virtual.VirtualExpUniverse;
 import edu.usf.ratsim.support.Configuration;
 import edu.usf.ratsim.support.ElementWrapper;
 import edu.usf.ratsim.support.XMLDocReader;
@@ -142,55 +139,55 @@ public class Experiment implements Runnable {
 	}
 
 	public void execPlottingScripts() {
-		// try {
-		// // Copy the maze to the experiment's folder
-		// FileUtils.copyURLToFile(getClass().getResource(mazeFile), new File(
-		// getLogPath() + "/maze.xml"));
-		// // Copy the plotting script to the experiment's folder
-		// FileUtils.copyURLToFile(getClass().getResource(PLOTTING_SCRIPT),
-		// new File(getLogPath() + "/plotting.r"));
-		// FileUtils.copyURLToFile(getClass().getResource(OBJ2PNG_SCRIPT),
-		// new File(getLogPath() + "/obj2png.r"));
-		// } catch (IOException e) {
-		// // TODO Auto-generated catch block
-		// e.printStackTrace();
-		// }
-		//
-		// // Copy the plotting script to the experiment's folder
-		// try {
-		// FileUtils.copyURLToFile(getClass().getResource(PLOT_EXECUTER),
-		// new File(getLogPath() + "/plot.sh"));
-		// } catch (IOException e) {
-		// // TODO Auto-generated catch block
-		// e.printStackTrace();
-		// }
-		//
-		// // Execute the plotting script
-		// try {
-		// System.out.println("Executing plotting scripts");
-		// Process plot = Runtime.getRuntime().exec("sh plot.sh", null,
-		// new File(getLogPath()));
-		// BufferedReader in = new BufferedReader(new InputStreamReader(
-		// plot.getInputStream()));
-		// String line = null;
-		// while ((line = in.readLine()) != null) {
-		// System.out.println(line);
-		// }
-		//
-		// BufferedReader err = new BufferedReader(new InputStreamReader(
-		// plot.getErrorStream()));
-		// line = null;
-		// while ((line = err.readLine()) != null) {
-		// System.out.println(line);
-		// }
-		// plot.waitFor();
-		// } catch (IOException e) {
-		// // TODO Auto-generated catch block
-		// e.printStackTrace();
-		// } catch (InterruptedException e) {
-		// // TODO Auto-generated catch block
-		// e.printStackTrace();
-		// }
+		 try {
+		 // Copy the maze to the experiment's folder
+		 FileUtils.copyURLToFile(getClass().getResource(mazeFile), new File(
+		 getLogPath() + "/maze.xml"));
+		 // Copy the plotting script to the experiment's folder
+		 FileUtils.copyURLToFile(getClass().getResource(PLOTTING_SCRIPT),
+		 new File(getLogPath() + "/plotting.r"));
+		 FileUtils.copyURLToFile(getClass().getResource(OBJ2PNG_SCRIPT),
+		 new File(getLogPath() + "/obj2png.r"));
+		 } catch (IOException e) {
+		 // TODO Auto-generated catch block
+		 e.printStackTrace();
+		 }
+		
+		 // Copy the plotting script to the experiment's folder
+		 try {
+		 FileUtils.copyURLToFile(getClass().getResource(PLOT_EXECUTER),
+		 new File(getLogPath() + "/plot.sh"));
+		 } catch (IOException e) {
+		 // TODO Auto-generated catch block
+		 e.printStackTrace();
+		 }
+		
+		 // Execute the plotting script
+		 try {
+		 System.out.println("Executing plotting scripts");
+		 Process plot = Runtime.getRuntime().exec("sh plot.sh", null,
+		 new File(getLogPath()));
+			BufferedReader in = new BufferedReader(new InputStreamReader(
+		 plot.getInputStream()));
+		 String line = null;
+		 while ((line = in.readLine()) != null) {
+		 System.out.println(line);
+		 }
+		
+		 BufferedReader err = new BufferedReader(new InputStreamReader(
+		 plot.getErrorStream()));
+		 line = null;
+		 while ((line = err.readLine()) != null) {
+		 System.out.println(line);
+		 }
+		 plot.waitFor();
+		 } catch (IOException e) {
+		 // TODO Auto-generated catch block
+		 e.printStackTrace();
+		 } catch (InterruptedException e) {
+		 // TODO Auto-generated catch block
+		 e.printStackTrace();
+		 }
 
 	}
 
