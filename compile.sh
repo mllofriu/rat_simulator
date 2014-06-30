8c52ca76e0e7a9eec6e71b41dc4f85e22f296c94
mllofriu <mllofriu@gmail.com>
2014-06-30 18:19:02 -0400

    execution on circe scripts fixed

--- compile.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rm -r bin
-mkdir bin
-javac -sourcepath src/:deps/classes/ -d bin -classpath ".:/home/m/mllofriualon//java_libs/commons-io-2.4.jar:/home/m/mllofriualon//java_libs/j3dcore.jar:/home/m/mllofriualon//java_libs/j3dutils.jar:/home/m/mllofriualon//java_libs/tcljava1.4.1/jacl.jar:/home/m/mllofriualon//java_libs/tcljava1.4.1/tcljava.jar:/home/m/mllofriualon//java_libs/vecmath.jar:deps/classes/nslj/src/" `find ./src/ -iname *.java`
---
+++ scripts/compile.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+rm -r bin
+mkdir bin
+javac -sourcepath /home/m/mllofriualon/rat_simulator/src/:/home/m/mllofriualon/rat_simulator/deps/classes/ -d bin -classpath ".:/home/m/mllofriualon//java_libs/commons-io-2.4.jar:/home/m/mllofriualon//java_libs/j3d-1_5_2-linux-amd64/j3dcore.jar:/home/m/mllofriualon//java_libs/j3d-1_5_2-linux-amd64/j3dutils.jar:/home/m/mllofriualon//java_libs/tcljava1.4.1/jacl.jar:/home/m/mllofriualon//java_libs/tcljava1.4.1/tcljava.jar:/home/m/mllofriualon//java_libs/j3d-1_5_2-linux-amd64/vecmath.jar:/home/m/mllofriualon//java_libs/jts-1.8.jar:java_libs/classes/nslj/src/" `find /home/m/mllofriualon/rat_simulator/src/ -iname *.java`
