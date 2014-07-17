469d12116e1ceb33f366055e81a30977e0b5c63a
Martin Llofriu <mllofriu@gmail.com>
2014-07-17 17:24:56 -0400

    changed execution to run on biorob cluster
    Former-commit-id: 6050bcaaa7702fb99977951723e36cb8efe6a5da

--- scripts/exec.sh
+++ scripts/exec.sh
@@ -1,20 +1,27 @@
 #$ -cwd
 #$ -N rat_sim
-#$ -l h_rt=00:20:00,pcpus=1,mpj=200M
+# #$ -l h_rt=01:00:00,pcpus=1,mpj=200M
 #$ -m b
 #$ -m e 
-#$ -M mllofriualon@mail.usf.edu
+# #$ -M mllofriualon@mail.usf.edu
 # #$ -t 1-576
 
 experiment=$1
 logDir=$2
 numIndividuals=$3
 
+RATSIM=/work/rat_simulator/
+JAVA_LIBS=/work/java_libs/
+JAVA=java
+#JAVA=/etc/alternatives/java_sdk_1.7.0/bin/java
 #sh scripts/compile.sh
 
-Xvfb :1 -screen 2 1600x1200x16 +extension GLX &
+export PATH=$PATH:/work/R-3.1.1/bin
+
+Xvfb :$numIndividuals -screen 2 1600x1200x16 +extension GLX &
 xvfb_pid=$!
-export DISPLAY=:1.2 
-/etc/alternatives/java_sdk_1.7.0/bin/java  -Xmx16000m -Djava.library.path=/home/m/mllofriualon/java_libs/j3d-1_5_2-linux-amd64/ -cp .:/home/m/mllofriualon/java_libs/nslj.jar:/home/m/mllofriualon//java_libs/commons-io-2.4.jar:/home/m/mllofriualon//java_libs/j3d-1_5_2-linux-amd64/j3dcore.jar:/home/m/mllofriualon//java_libs/j3d-1_5_2-linux-amd64/j3dutils.jar:/home/m/mllofriualon//java_libs/tcljava1.4.1/jacl.jar:/home/m/mllofriualon//java_libs/tcljava1.4.1/tcljava.jar:/home/m/mllofriualon//java_libs/jts-1.8.jar:/home/m/mllofriualon//java_libs/j3d-1_5_2-linux-amd64/vecmath.jar:bin/:src/ edu.usf.ratsim.experiment.Experiment $experiment $logDir $numIndividuals # $SGE_TASK_ID
+export DISPLAY=:$numIndividuals.2 
+#export DISPLAY=0:0
+$JAVA  -Xmx16000m -Djava.library.path=$JAVA_LIBS/j3d-1_5_2-linux-amd64/ -cp .:$JAVA_LIBS/nslj.jar:$JAVA_LIBS/commons-io-2.4.jar:$JAVA_LIBS/j3d-1_5_2-linux-amd64/j3dcore.jar:$JAVA_LIBS/j3d-1_5_2-linux-amd64/j3dutils.jar:$JAVA_LIBS/tcljava1.4.1/jacl.jar:$JAVA_LIBS/tcljava1.4.1/tcljava.jar:$JAVA_LIBS/jts-1.8.jar:$JAVA_LIBS/j3d-1_5_2-linux-amd64/vecmath.jar:bin/:src/ edu.usf.ratsim.experiment.Experiment $experiment $logDir $numIndividuals # $SGE_TASK_ID
 
 kill $xvfb_pid
