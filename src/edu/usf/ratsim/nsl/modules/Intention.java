b779c0ad7ed8fea45d6d283b8390f425f8497a69
Martin Llofriu <mllofriu@gmail.com>
2014-08-05 12:40:54 -0400

    Fixed the goal selection schema.
    Former-commit-id: 4dd47343765f056f41278fbcd19d47ac33648a2c

--- src/edu/usf/ratsim/nsl/modules/Intention.java
+++ src/edu/usf/ratsim/nsl/modules/Intention.java
@@ -1,5 +1,6 @@
 package edu.usf.ratsim.nsl.modules;
 
+import edu.usf.ratsim.support.Debug;
 import nslj.src.lang.NslDinInt0;
 import nslj.src.lang.NslDoutFloat1;
 import nslj.src.lang.NslModule;
@@ -21,9 +22,11 @@ public class Intention extends NslModule {
 
 		if (goalFeeder.get() != -1)
 			intention.set(goalFeeder.get(), 1);
-//		 for(int i = 0; i < intention.getSize(); i++)
-//			 System.out.print(intention.get(i) + " ");
-//		 System.out.println();
+		if (Debug.printIntention) {
+			for (int i = 0; i < intention.getSize(); i++)
+				System.out.print(intention.get(i) + " ");
+			System.out.println();
+		}
 
 	}
 }
