a892034e9004e3086ec173c6590560c6207d8917
Martin Llofriu <mllofriu@gmail.com>
2014-08-06 11:39:22 -0400

    Got layers back.
    Added something to end episodes after eating.
    
    Added debug class to control printing.

---
+++ src/edu/usf/ratsim/support/Debug.java
@@ -0,0 +1,13 @@
+package edu.usf.ratsim.support;
+
+public class Debug {
+
+	public static boolean printValues = true;
+	public static boolean printEndCycle = false;
+	public static boolean printAnyGoal = true;
+	public static boolean printTaxic = false;
+	public static boolean printActiveGoal = false;
+	public static boolean printRobotAte = true;
+	public static boolean printIntention = true;
+
+}
