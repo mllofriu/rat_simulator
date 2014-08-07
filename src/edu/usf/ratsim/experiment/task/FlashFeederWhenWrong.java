78b319e73a3a69f5b03f2fd32fffc268d0a90039
Martin Llofriu <mllofriu@gmail.com>
2014-08-06 20:00:35 -0400

    modified found N feeders no mistakes to include timeout
    Former-commit-id: f1be0f28730fbf5a3875f40d804956324938dd0f

--- src/edu/usf/ratsim/experiment/task/FlashFeederWhenWrong.java
+++ src/edu/usf/ratsim/experiment/task/FlashFeederWhenWrong.java
@@ -22,34 +22,6 @@ public class FlashFeederWhenWrong implements ExperimentTask {
 	}
 
 	public void perform(ExperimentUniverse univ, ExpSubject subject) {
-		// // If the robot tried to eat from an incorrect feeder
-		// if (univ.hasRobotTriedToEat()
-		// && (univ.getFeedingFeeder() == -1)
-		// && univ.getFlashingFeeders().isEmpty()) {
-		// // List<Integer> active = (List<Integer>) subject
-		// // .getProperty(ActivateFeeders.STR_ACTIVE_FEEDERS);
-		// List<Integer> active = univ.getActiveFeeders();
-		// active = new LinkedList<Integer>(active);
-		// int feeder = active.get(r.nextInt(active.size()));
-		// univ.setFlashingFeeder(feeder, true);
-		// univ.setActiveFeeder(feeder, true);
-		// } else if (!univ.getFlashingFeeders().isEmpty()
-		// && univ.hasRobotFoundFeeder(univ.getFlashingFeeders().get(0))
-		// && univ.hasRobotAte()) {
-		// int flashingFeeder = univ.getFlashingFeeders().get(0);
-		// univ.setFlashingFeeder(flashingFeeder, false);
-		// // univ.setActiveFeeder(flashingFeeder, false);
-		// // List<Integer> selectedFeeders = (List<Integer>) subject
-		// // .getProperty(ActivateFeeders.STR_ACTIVE_FEEDERS);
-		// // // Copy to avoid modifying the property holder
-		// // selectedFeeders = new LinkedList<Integer>(selectedFeeders);
-		// // if (selectedFeeders.contains(flashingFeeder))
-		// // selectedFeeders.remove(new Integer(flashingFeeder));
-		// // int feeder =
-		// // selectedFeeders.get(r.nextInt(selectedFeeders.size()));
-		// // univ.setFlashingFeeder(feeder, true);
-		// // univ.setActiveFeeder(feeder, true);
-		// }
 		if (univ.hasRobotAte()) {
 			stepsSinceLastAte = 0;
 			if (!univ.getFlashingFeeders().isEmpty())
