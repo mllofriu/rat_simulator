5653d38e7cc9542bfa4aeb059589c4ea39582132
mllofriu <mllofriu@gmail.com>
2014-08-11 18:48:00 -0400

    slurm execution
    Former-commit-id: 02b9a48b358dc134739ccf359f29a2acbdbb2b50

--- scripts/runslurm.sh
+++ scripts/runslurm.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-sbatch scripts/execslurm.sh /edu/usf/ratsim/experiment/xml/multiFeeders.xml Experiment
+if [ "$#" -ne 1 ]; then
+    echo "Illegal number of parameters"
+fi
+
+log=$1
+
+./scripts/compile.sh
+
+sbatch scripts/execslurm.sh /edu/usf/ratsim/experiment/xml/multiFeeders.xml $log
