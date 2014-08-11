f538ff10d55aab02a4af10e58d1855d1f2e6adec
mllofriu <mllofriu@gmail.com>
2014-08-11 18:48:00 -0400

    slurm execution
    Former-commit-id: 02b9a48b358dc134739ccf359f29a2acbdbb2b50

--- scripts/execslurm.sh
+++ scripts/execslurm.sh
@@ -2,11 +2,13 @@
 
 #SBATCH --array=1-576
 
+groupSize=64
 
 experiment=$1
 logDir=$2
 #numIndividuals=$3
-numIndividual=$SLURM_ARRAY_TASK_ID
+numIndividual=`echo $SLURM_ARRAY_TASK_ID % $groupSize| bc`
+numGroup=`echo $SLURM_ARRAY_TASK_ID / $groupSize | bc`
 RATSIM=/work/rat_simulator/
 JAVA_LIBS=/work/java_libs/
 
@@ -19,10 +21,10 @@ JAVA=/etc/alternatives/java_sdk_1.7.0/bin/java
 
 export PATH=/work/R-3.1.1/bin:$PATH
 
-Xvfb :$numIndividual -screen 2 1600x1200x16 +extension GLX &
+Xvfb :$SLURM_ARRAY_TASK_ID -screen 2 1600x1200x16 +extension GLX &
 xvfb_pid=$!
-export DISPLAY=:$numIndividual.2 
+export DISPLAY=:$SLURM_ARRAY_TASK_ID.2 
 #export DISPLAY=0:0
-$JAVA  -Xmx16000m -Djava.library.path=$JAVA_LIBS/j3d-1_5_2-linux-amd64/ -cp .:$JAVA_LIBS/nslj.jar:$JAVA_LIBS/commons-io-2.4.jar:$JAVA_LIBS/j3d-1_5_2-linux-amd64/j3dcore.jar:$JAVA_LIBS/j3d-1_5_2-linux-amd64/j3dutils.jar:$JAVA_LIBS/tcljava1.4.1/jacl.jar:$JAVA_LIBS/tcljava1.4.1/tcljava.jar:$JAVA_LIBS/jts-1.8.jar:$JAVA_LIBS/j3d-1_5_2-linux-amd64/vecmath.jar:bin/:src/ edu.usf.ratsim.experiment.Experiment $experiment $logDir $numIndividual # $SGE_TASK_ID
+$JAVA  -Xmx16000m -Djava.library.path=$JAVA_LIBS/j3d-1_5_2-linux-amd64/ -cp .:$JAVA_LIBS/nslj.jar:$JAVA_LIBS/commons-io-2.4.jar:$JAVA_LIBS/j3d-1_5_2-linux-amd64/j3dcore.jar:$JAVA_LIBS/j3d-1_5_2-linux-amd64/j3dutils.jar:$JAVA_LIBS/tcljava1.4.1/jacl.jar:$JAVA_LIBS/tcljava1.4.1/tcljava.jar:$JAVA_LIBS/jts-1.8.jar:$JAVA_LIBS/j3d-1_5_2-linux-amd64/vecmath.jar:bin/:src/ edu.usf.ratsim.experiment.Experiment $experiment $logDir $numGroup $numIndividual # $SGE_TASK_ID
 
 kill $xvfb_pid
