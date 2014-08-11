6dd76695098f3a59a79ef7cb0c740e9060a24e67
Martin Llofriu <mllofriu@gmail.com>
2014-08-11 13:30:49 -0400

    debugging changes
    Former-commit-id: 3c80207d358dfefa0324e90bda590416badb12b7

--- src/edu/usf/ratsim/support/Debug.java
+++ src/edu/usf/ratsim/support/Debug.java
@@ -2,7 +2,7 @@ package edu.usf.ratsim.support;
 
 public class Debug {
 
-	public static boolean printValues = false;
+	public static boolean printValues = true;
 	public static boolean printEndCycle = false;
 	public static boolean printAnyGoal = false;
 	public static boolean printTaxic = false;
@@ -13,8 +13,9 @@ public class Debug {
 	public static boolean printFoundNNoMistakes = false;
 	
 	
-	public static boolean waitBeforeTrial = false;
+	public static boolean waitBeforeTrial = true;
 	public static boolean moveRobot = true;
+	public static boolean sleepBeforeStart = false;	
 
 
 }
