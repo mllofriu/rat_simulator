8739c9684ea4995dfb266f075b389c55ec1b76ca
mllofriu <mllofriu@gmail.com>
2014-08-13 15:45:12 -0400

    execution dependencies with slurm

--- scripts/plotRuntimes.sh
+++ scripts/plotRuntimes.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+#SBATCH -J ratsim
+#SBATCH --dependency=singleton
+
+export PATH=/home/m/mllofriualon/R-3.0.2/bin:$PATH
+
 logdir=$1
 
 cp src/edu/usf/ratsim/experiment/plot/multifeeders/plotRuntimes.r logs/$logdir/
