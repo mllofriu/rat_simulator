5e4f9ae38d00e40502b236dda51e5fa2542bbe71
mllofriu <mllofriu@gmail.com>
2014-08-13 15:45:12 -0400

    execution dependencies with slurm
    Former-commit-id: e1ff706fbee6fb162b8e3298d73dee7f2051454d

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+#SBATCH -J ratsim
+
 #RATSIM=/work/rat_simulator/
 #JAVA_LIBS=/work/java_libs/
 
-RATSIM=/home/m/mllofriualon/work/rat_simulator
+#RATSIM=/home/m/mllofriualon/work/rat_simulator
+#JAVA_LIBS=/home/m/mllofriualon/java_libs/
+
+RATSIM=/home/m/mllofriualon/rat_simulator
 JAVA_LIBS=/home/m/mllofriualon/java_libs/
 JAVAC=/etc/alternatives/java_sdk_1.7.0/bin/javac
 
--- scripts/execslurm.sh
+++ scripts/execslurm.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 #SBATCH --array=1-576
+#SBATCH -J ratsim
+# #SBATCH --dependency=singleton
 
 # execute ploting after this job
-sbatch --dependency=afterok:$SLURM_JOB_ID scripts/plotRuntimes.sh $log
+#sbatch --dependency=afterok:$SLURM_JOB_ID scripts/plotRuntimes.sh $log
 
 groupSize=64
 
@@ -12,17 +14,19 @@ logDir=$2
 #numIndividuals=$3
 numIndividual=`echo $SLURM_ARRAY_TASK_ID % $groupSize| bc`
 numGroup=`echo $SLURM_ARRAY_TASK_ID / $groupSize | bc`
-RATSIM=/work/rat_simulator/
-JAVA_LIBS=/work/java_libs/
 
-RATSIM=/home/m/mllofriualon/work/rat_simulator
+#RATSIM=/home/m/mllofriualon/work/rat_simulator
+#JAVA_LIBS=/home/m/mllofriualon/java_libs/
+
+RATSIM=/home/m/mllofriualon/rat_simulator
 JAVA_LIBS=/home/m/mllofriualon/java_libs/
 
 JAVA=java
 JAVA=/etc/alternatives/java_sdk_1.7.0/bin/java
 #sh scripts/compile.sh
 
-export PATH=/work/R-3.1.1/bin:$PATH
+#export PATH=/work/R-3.1.1/bin:$PATH
+export PATH=/home/m/mllofriualon/R-3.0.2/bin:$PATH
 
 Xvfb :$SLURM_ARRAY_TASK_ID -screen 2 1600x1200x16 +extension GLX &
 xvfb_pid=$!
