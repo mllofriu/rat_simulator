7f911117d54e6f5e5a52674fee6db3955f51be49
Martin Llofriu <mllofriu@gmail.com>
2014-08-19 13:00:05 -0400

    Fixed give food to avoid giving twice. Seems to learn no to eat from invalid feeders.
    Former-commit-id: ee45035fa6f7e13adc79cb6eab16e8743beb5a07

--- src/edu/usf/ratsim/support/Debug.java
+++ src/edu/usf/ratsim/support/Debug.java
@@ -6,16 +6,16 @@ public class Debug {
 	public static boolean moveRobot = true;
 	public static boolean sleepBeforeStart = false;
 	
-	public static boolean printValues = true;
-	public static boolean printEndCycle = true;
+	public static boolean printValues = false;
+	public static boolean printEndCycle = false;
 	public static boolean printAnyGoal = false;
 	public static boolean printTaxic = false;
 	public static boolean printActiveGoal = false;
 	public static boolean printRobotAte = false;
 	public static boolean printIntention = false;
-	public static boolean printTryingToEat = true;
+	public static boolean printTryingToEat = false;
 	public static boolean printFoundNNoMistakes = false;
 	public static boolean printHDCells = false;
-	public static boolean printGiveFood = false;	
+	public static boolean printGiveFood = true;	
 	
 }
