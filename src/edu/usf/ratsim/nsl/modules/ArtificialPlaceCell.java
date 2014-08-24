4d0e29abb0da7e5dc8c8ec3f2fda86f22518a611
Martin Llofriu <mllofriu@gmail.com>
2014-08-24 12:11:43 -0400

    Changed way of specifiifying cell width
    Former-commit-id: bc59db850d96bc12e7ed5af23746d39e1728d471

--- src/edu/usf/ratsim/nsl/modules/ArtificialPlaceCell.java
+++ src/edu/usf/ratsim/nsl/modules/ArtificialPlaceCell.java
@@ -4,12 +4,14 @@ import javax.vecmath.Point3f;
 
 public class ArtificialPlaceCell {
 
+	private static final double RADIUS_THRS = 0.2;
 	private Point3f center;
 	private float width;
 
 	public ArtificialPlaceCell(Point3f center, float radius) {
 		this.center = center;
-		this.width = radius*2;
+		// min_thrs = e^(-x_min_thrs^2/w) -> ...
+		this.width = (float) (- Math.pow(radius,2) / Math.log(RADIUS_THRS));
 	}
 
 //	public boolean isActive(Point3f currLocation) {
