fc7313b829fc162b04c8da8fcc2623adf6b7a5b2
Martin Llofriu <mllofriu@gmail.com>
2015-01-22 19:51:58 -0500

    Romina robot, first adaptation

---
+++ src/edu/usf/ratsim/robot/romina/Romina.java
@@ -0,0 +1,182 @@
+package edu.usf.ratsim.robot.romina;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.vecmath.Point3f;
+
+import edu.usf.ratsim.experiment.ExperimentUniverse;
+import edu.usf.ratsim.robot.IRobot;
+import edu.usf.ratsim.robot.Landmark;
+import edu.usf.ratsim.robot.naorobot.protobuf.Connector.Command;
+import edu.usf.ratsim.robot.naorobot.protobuf.Connector.Command.Builder;
+import edu.usf.ratsim.robot.naorobot.protobuf.Connector.Command.CommandType;
+import edu.usf.ratsim.robot.naorobot.protobuf.Connector.Response;
+import edu.usf.ratsim.robot.virtual.ExpUniverseNode;
+import edu.usf.ratsim.robot.virtual.UniverseFrame;
+import edu.usf.ratsim.robot.virtual.VirtualExpUniverse;
+import edu.usf.ratsim.support.Configuration;
+
+public class Romina implements IRobot {
+
+	private Socket protoSocket;
+
+	public Romina(String host, int port, ExperimentUniverse world) {
+		if (Configuration.getBoolean("UniverseFrame.display")) {
+			UniverseFrame worldFrame = new UniverseFrame((VirtualExpUniverse) world);
+			worldFrame.setVisible(true);
+		}
+		
+		((SLAMUniverse)world).setRominaRobot(this);
+		
+		try {
+			protoSocket = new Socket(host, port);
+			System.out.println("Connection stablished");
+
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		try {
+			Builder b = Command.newBuilder();
+			b.setType(CommandType.startRobot);
+			Command c = b.build();
+			c.writeTo(protoSocket.getOutputStream());
+
+			Response.parseDelimitedFrom(protoSocket.getInputStream());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+	@Override
+	public void rotate(float degrees) {
+		try {
+			Builder b = Command.newBuilder();
+			b.setType(CommandType.doAction);
+			b.setAngle(degrees);
+			Command c = b.build();
+			c.writeTo(protoSocket.getOutputStream());
+
+			Response.parseDelimitedFrom(protoSocket.getInputStream());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void eat() {
+	}
+
+	@Override
+	public boolean[] getAffordances() {
+		try {
+			Builder b = Command.newBuilder();
+			b.setType(CommandType.getInfo);
+			Command c = b.build();
+			c.writeTo(protoSocket.getOutputStream());
+
+			Response r = Response.parseDelimitedFrom(protoSocket
+					.getInputStream());
+
+			boolean res[] = new boolean[r.getAffs().getAffCount()];
+			for (int i = 0; i < r.getAffs().getAffCount(); i++)
+				res[i] = r.getAffs().getAff(i);
+
+			return res;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+
+	@Override
+	public boolean hasFoundFood() {
+		return false;
+	}
+
+	@Override
+	public void startRobot() {
+		try {
+			Builder b = Command.newBuilder();
+			b.setType(CommandType.startRobot);
+			Command c = b.build();
+			c.writeTo(protoSocket.getOutputStream());
+
+			Response.parseDelimitedFrom(protoSocket.getInputStream());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public BufferedImage[] getPanoramica() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void forward() {
+		rotate(0);
+	}
+
+	@Override
+	public boolean[] getAffordances(int wallLookahead) {
+		return getAffordances();
+	}
+
+	@Override
+	public List<Landmark> getLandmarks() {
+		try {
+			Builder b = Command.newBuilder();
+			b.setType(CommandType.getInfo);
+			Command c = b.build();
+			c.writeTo(protoSocket.getOutputStream());
+
+			Response r = Response.parseDelimitedFrom(protoSocket
+					.getInputStream());
+
+			List<Landmark> lms = new LinkedList<Landmark>();
+			for (edu.usf.ratsim.robot.naorobot.protobuf.Connector.Landmark lm : r
+					.getLandmarksList())
+				lms.add(new Landmark(lm));
+
+			return lms;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+
+	@Override
+	public boolean hasTriedToEat() {
+		return false;
+	}
+
+	public Point3f getRobotPoint() {
+		return new Point3f(0,0,0);
+	}
+
+	public float getRobotOrientation() {
+		return 0;
+	}
+
+
+}
