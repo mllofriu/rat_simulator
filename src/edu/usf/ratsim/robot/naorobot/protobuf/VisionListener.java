80e19fa5e3d76bc9c30e2ba54074558b5780555a
Martin Llofriu <mllofriu@gmail.com>
2015-01-30 14:51:05 -0500

    Auto calibrating system

--- src/edu/usf/ratsim/robot/naorobot/protobuf/VisionListener.java
+++ src/edu/usf/ratsim/robot/naorobot/protobuf/VisionListener.java
@@ -27,21 +27,20 @@ import edu.usf.ratsim.support.Position;
 
 public class VisionListener {
 
-	private final float nex = 414;
-	private final float ney = 1270;
-	private final float sex = 2740;
-	private final float sey = 1390;
-	private final float nwx = 396;
-	private final float nwy = -1020;
-	private final float swx = 2719;
-	private final float swy = -1020;
+	private final float nex = 171.66917f;
+	private final float ney = 1245.7004f;
+	private final float sex = 2501.2043f;
+	private final float sey = 1313.967f;
+	private final float swx = 2606.3542f;
+	private final float swy = -1016.0847f;
+	private final float nwx = 284.65808f;
+	private final float nwy = -1033.0831f;
 	private final float fl = 2;
-	
+
 	private InetAddress group;
 	private MulticastSocket s;
 	private Position lastPosition;
 	private long lastPosTime;
-	
 
 	public VisionListener() {
 		try {
@@ -64,7 +63,7 @@ public class VisionListener {
 	private Position getRobotPosition() {
 		if (Math.abs(System.currentTimeMillis() - lastPosTime) < 200)
 			return lastPosition;
-		
+
 		boolean finish = false;
 		SSL_WrapperPacket f = null;
 		while (!finish) {
@@ -76,7 +75,7 @@ public class VisionListener {
 				System.arraycopy(buf, 0, data, 0, recv.getLength());
 				f = SSL_WrapperPacket.parseFrom(data);
 				finish = f.getDetection().getRobotsBlueList().size() > 0;
-				
+
 			} catch (UnknownHostException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -87,14 +86,13 @@ public class VisionListener {
 		}
 
 		SSL_DetectionRobot r = f.getDetection().getRobotsBlue(0);
-//		System.out.println(r.getX());
-//		System.out.println(r.getY());
+		// System.out.println(r.getX());
+		// System.out.println(r.getY());
 		lastPosition = new Position(r.getX(), r.getY(), r.getOrientation());
 		lastPosTime = System.currentTimeMillis();
 		return lastPosition;
 	}
-	
-	
+
 	@Override
 	protected void finalize() throws Throwable {
 		super.finalize();
@@ -106,32 +104,69 @@ public class VisionListener {
 		Position p = getRobotPosition();
 		return scale(new Point3f(p.getX(), p.getY(), 0));
 	}
-	
+
 	private Point3f scale(Point3f p) {
-		float x = (-fl/(nwx - swx)) * p.x + (1 + (fl/(nwx - swx)) * swx);
-		float y = (fl/(ney - nwy)) * p.y + (-1 + (-fl/(ney - nwy)) * nwy);
-		x = Math.min (fl/2, x); x = Math.max(-fl/2, x);
-		y = Math.min(fl/2, y); y = Math.max(-fl/2, y);
+		float x = (-fl / (nwx - swx)) * p.x + (1 + (fl / (nwx - swx)) * swx);
+		float y = (fl / (ney - nwy)) * p.y + (-1 + (-fl / (ney - nwy)) * nwy);
+		x = Math.min(fl / 2, x);
+		x = Math.max(-fl / 2, x);
+		y = Math.min(fl / 2, y);
+		y = Math.max(-fl / 2, y);
+//		System.out.println(x + " " + y);
 		return new Point3f(x, y, 0);
 	}
 
-	public float getRobotOrientation(){
+	public float getRobotOrientation() {
 		Position p = getRobotPosition();
-		return -p.getOrient();
+//		System.out.println(p.getOrient());
+		return p.getOrient();
 	}
-	
-	public static void main(String[] args){
-		VisionListener v = new VisionListener();
-		while (true){
-			System.out.println(v.getRobotPoint());
-			try {
-				Thread.sleep(100);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+
+	public static void main(String[] args) {
+		// VisionListener v = new VisionListener();
+		// while (true){
+		// System.out.println(v.getRobotPoint());
+		// try {
+		// Thread.sleep(100);
+		// } catch (InterruptedException e) {
+		// // TODO Auto-generated catch block
+		// e.printStackTrace();
+		// }
+		// }
+		Position p = null;
+		try {
+			VisionListener v = new VisionListener();
+			
+			for (int i = 0; i < 10; i++){
+				p = v.getRobotPosition();
+				Thread.sleep(1000);
 			}
+			System.out.println("private final float nex = " + p.getX() + "f;");
+			System.out.println("private final float ney = " + p.getY() + "f;");
+			Thread.sleep(10000);
+			for (int i = 0; i < 10; i++){
+				p = v.getRobotPosition();
+				Thread.sleep(1000);
+			}
+			System.out.println("private final float sex = " + p.getX() + "f;");
+			System.out.println("private final float sey = " + p.getY() + "f;");
+			Thread.sleep(10000);
+			for (int i = 0; i < 10; i++){
+				p = v.getRobotPosition();
+				Thread.sleep(1000);
+			}
+			System.out.println("private final float swx = " + p.getX() + "f;");
+			System.out.println("private final float swy = " + p.getY() + "f;");
+			Thread.sleep(10000);
+			for (int i = 0; i < 10; i++){
+				p = v.getRobotPosition();
+				Thread.sleep(1000);
+			}
+			System.out.println("private final float nwx = " + p.getX() + "f;");
+			System.out.println("private final float nwy = " + p.getY() + "f;");
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		
-//		System.out.println(new VisionListener().getRobotOrientation());
 	}
 }
