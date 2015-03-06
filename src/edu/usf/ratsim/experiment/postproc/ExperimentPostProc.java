ee2d9c481a9f21d132d65465dbb95eba6a39e59c
Martin Llofriu <mllofriu@gmail.com>
2015-03-06 14:54:10 -0500

    Deleted old stuff

--- src/edu/usf/ratsim/experiment/postproc/ExperimentPostProc.java
+++
@@ -1,7 +0,0 @@
-package edu.usf.ratsim.experiment.postproc;
-
-public interface ExperimentPostProc {
-
-	void perform();
-
-}
--- src/edu/usf/ratsim/experiment/subject/initializer/SubjectInitializer.java
+++
@@ -1,8 +0,0 @@
-package edu.usf.ratsim.experiment.subject.initializer;
-
-
-public interface SubjectInitializer {
-	
-	public void initializeSubject(ExpSubject subject);
-
-}
